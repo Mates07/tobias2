ddd0d4a805ede9631d2e5e393d871c71576512d9
Matej Dolecek <1179@adc.itgymnazium.cz>
2023-12-05 09:32:22 +0100

    1179_cykly

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+string  x  =  Console.ReadLine();
+string  y  =  Console.ReadLine();
+if  (x.Length  >  y.Length)
+{
+        Console.WriteLine(x);
+}
+else  if  (y.Length  >  x.Length)
+{
+        Console.WriteLine(y);
+}
+else
+{
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+}
+*/
+/*  bool  proced  =  true;
+while  (proced)
+{
+        Console.WriteLine("zadejte  text");
+        string  userInput  =  Console.ReadLine();
+        if  (string.IsNullOrWhiteSpace(userInput))
+        {
+                proced  =  false
+        }
+        else
+        {
+                Console.WriteLine($"zadaný  text  má  délku:  {userInput.Length}");
+        }
+        
+}
+*/
+/*
+Console.WriteLine("zadejte  text");
+string a = Console.ReadLine();
+int count = 4;
+    while (count >= 0)
+    {
+        Console.WriteLine(a);
+        count--;
+    }
+*/
+/*
+var c = 6;
+var d = 20;
+while (c < d)
+{
+    c++; 
+}
+Console.WriteLine(c);
+*/
+var a = Console.ReadLine(); 
+for (int i = 5; i > 0; i--)
+{
+    Console.WriteLine(a);
+}
+
